99c9530ea6c13c39a93c1d7f46cbcdd582c55ffc
tstanvir <tanvirshossain0@gmail.com>
2018-09-10 18:11:00 +0600

    Add solution for problem `A - Again Twenty Five!` Link: https://codeforces.com/contest/630/problem/A Tags: number theory, *800 Ref: https://codeforces.com/contest/630/submission/42732236

--- codeforces/630/A.cpp
+++ codeforces/630/A.cpp
@@ -2,36 +2,26 @@
 
 using namespace std;
 
-const long long MOD = 1000000007;
+#define PI 3.1415926535897932384626433832795
+#define REP(i,n) for(int i=0;i<(n);i++)
 #define SET(x) memset(x, 0, sizeof(x))
-#define SET2d(x,m,n) memset(x, 0, sizeof(x[0][0]) * m * n)
-#define SETBOOL(x) memset(x,false,sizeof(x))
 #define CLR(x) memset(x, -1, sizeof(x))
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
 #define mp make_pair
-#define PII pair<int, int>
-#define pf printf
+#define p printf
 
-#define sf scanf
+#define s scanf
 
-#define ALL(x) x.begin(),x.end()
+#define ll long long
+
 #define pb push_back
 
-#define IOS ios::sync_with_stdio(false); cin.tie(0); 
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
 #define np std::string::npos
-#define for0(i,n) for(int i=0;i<n;i++)
-#define forn(i,n) for(int i=n-1;i>=0;i--)
-#define highest(x) numeric_limits<x>::max()
-#define lowest(x) numeric_limits<x>::min()
-#define Inf INFINITY
-#define minv(v) *min_element(v.begin(),v.end())
-#define maxv(v) *max_element(v.begin(),v.end())
-#define cases(cs,t) for(int cs=1;cs<=t;cs++)
-#define PI acos(-1)
-typedef long long ll;
-int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
-int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
-int dx4[] = {0, 0, 1, -1};
-int dy4[] = {1, -1, 0, 0};
+#define INF Ox3f3f3f3f
 
 //this fuction sorts vector pair according to first element in descending order.
 bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
@@ -39,26 +29,13 @@ bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
     return a.first>b.first;
 }
 
-template<typename T>inline T Bigmod(T base, T power, T MOD){
-    T ret=1;
-    while(power)
-    {
-        if(power & 1)ret=(ret*base)%MOD;
-        base=(base*base)%MOD;
-        power>>=1;
-    }
-    return ret;
-}
-
 int main()
 
 {
 	IOS;
-	//freopen("input.txt", "r", stdin);
-     //freopen("output.txt", "w", stdout);
-     ll n;
-     cin>>n;
-     cout<<25<<endl;
+	ll n;
+	cin>>n;
+	cout<<25<<endl;
 
 	 return 0;
 
