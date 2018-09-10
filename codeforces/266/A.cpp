16e1b3b6a5eab105582e3465aa37e637639fe087
tstanvir <tanvirshossain0@gmail.com>
2018-09-11 00:38:00 +0600

    Add solution for problem `A - Stones on the Table` Link: https://codeforces.com/contest/266/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/266/submission/42745142

---
+++ codeforces/266/A.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define PI 3.1415926535897932384626433832795
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define SET(x) memset(x, 0, sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define mp make_pair
+#define p printf
+
+#define s scanf
+
+#define ll long long
+
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+#define np std::string::npos
+#define INF 0x3f3f3f3f
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+int main()
+
+{
+	IOS;
+	int n;
+	string s;
+	cin>>n>>s;
+    int bal=0;
+	for(int i=0;i<n;i++)
+	{
+	    if(i+1<n && s[i]==s[i+1] ) bal++;
+
+	}
+    cout<<bal<<endl;
+
+	 return 0;
+
+}
+
