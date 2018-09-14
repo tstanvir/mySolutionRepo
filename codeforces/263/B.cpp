419466788a013a0304eb27077e631387bdcfb0c9
tstanvir <tanvirshossain0@gmail.com>
2018-09-15 00:23:00 +0600

    Add solution for problem `B - Squares` Link: https://codeforces.com/contest/263/problem/B Tags: greedy, implementation, sortings, *900 Ref: https://codeforces.com/contest/263/submission/42870222

---
+++ codeforces/263/B.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define PI 3.1415926535897932384626433832795
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define SET(x) memset(x, 0, sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define mp make_pair
+#define p printf
+
+#define s scanf
+
+#define ll long long
+
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+#define np std::string::npos
+#define INF Ox3f3f3f3f
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+int main()
+
+{
+	IOS;
+	int n,k;
+	while(cin>>n>>k)
+    {
+        ll arr[n+1];
+        for(int i=0;i<n;i++) cin>>arr[i];
+        if(k>n) {cout<<-1<<endl; continue;}
+        sort(arr,arr+n,greater<ll>());
+        ll x=arr[k-1];
+        cout<<x<<" "<<0<<endl;
+    }
+
+	 return 0;
+
+}
+
