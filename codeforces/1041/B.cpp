4bb1db7d10030b11bc82bcc59ef72111ac99dcc8
tstanvir <tanvirshossain0@gmail.com>
2018-09-16 17:05:00 +0600

    Add solution for problem `B - Buying a TV Set` Link: https://codeforces.com/contest/1041/problem/B Tags: math, *1000 Ref: https://codeforces.com/contest/1041/submission/42929029

--- codeforces/1041/B.cpp
+++ codeforces/1041/B.cpp
@@ -2,22 +2,50 @@
 
 using namespace std;
 
+#define PI 3.1415926535897932384626433832795
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define SET(x) memset(x, 0, sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define mp make_pair
+#define p printf
+
+#define s scanf
+
+#define ll long long
+
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+#define np std::string::npos
+#define INF Ox3f3f3f3f
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
 
 int main()
 
 {
-
-	long long a,b,x,y;
+	IOS;
+	ll a,b,x,y;
 	while(cin>>a>>b>>x>>y)
     {
-        long long  gc=__gcd(x,y);
+        ll bal=__gcd(x,y);
         //cout<<bal<<endl;
-        if(gc>0) {x=x/gc; y=y/gc;}
-        long long A,B;
-        A=a/x; B=b/y;
-        cout<<min(A,B)<<endl;
+        if(bal>0) {x=x/bal; y=y/bal;}
+        ll heda,beda;
+        heda=a/x; beda=b/y;
+        if(heda<beda) cout<<heda<<endl;
+        else cout<<beda<<endl;
     }
 
 	 return 0;
 
 }
+
