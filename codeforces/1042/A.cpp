de1cacb7da90013712dfbe1559920710e6855049
tstanvir <tanvirshossain0@gmail.com>
2018-09-18 04:17:00 +0600

    Add solution for problem `A - Benches` Link: https://codeforces.com/contest/1042/problem/A Tags: binary search, implementation, *1100 Ref: https://codeforces.com/contest/1042/submission/43015521

---
+++ codeforces/1042/A.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define PI 3.1415926535897932384626433832795
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define SET(x) memset(x, 0, sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define mp make_pair
+#define p printf
+
+#define s scanf
+
+#define ll long long
+
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+#define np std::string::npos
+#define INF Ox3f3f3f3f
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+int main()
+
+{
+	IOS;
+	int n,m;
+	while(cin>>n>>m)
+    {
+        int arr[n];
+        for(int i=0;i<n;i++) cin>>arr[i];
+        int* maxi=max_element(arr,arr+n);
+        int sum=accumulate(arr,arr+n,0);
+        int mini=ceil(((double)sum+(double)m)/(double)n);
+        cout<<max(mini,*maxi)<<" "<<*maxi+m<<endl;
+    }
+
+	 return 0;
+
+}
+
