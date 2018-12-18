c6664ebe7add5948a2d222009534a28ac251e367
tstanvir <tanvirshossain0@gmail.com>
2018-12-18 21:01:00 +0600

    Add solution for problem `B - Teams Forming` Link: https://codeforces.com/contest/1092/problem/B Tags: sortings, *800 Ref: https://codeforces.com/contest/1092/submission/47205442

---
+++ codeforces/1092/B.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define PI 3.1415926535897932384626433832795
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define SET(x) memset(x, 0, sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define mp make_pair
+#define p printf
+
+#define s scanf
+
+#define ll long long
+
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+#define np std::string::npos
+#define INF Ox3f3f3f3f
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+int main()
+
+{
+	IOS;
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++) cin>>arr[i];
+	sort(arr,arr+n);
+	int ans=0;
+	for(int i=1;i<n;i+=2)
+        ans+=arr[i]-arr[i-1];
+    cout<<ans<<endl;
+
+	 return 0;
+
+}
+
