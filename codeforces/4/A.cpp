ca37125e49c41a3c2fd73cb2aadf267cb2e77a1b
tstanvir <tanvirshossain0@gmail.com>
2019-01-03 22:14:00 +0600

    Add solution for problem `A - Watermelon` Link: https://codeforces.com/contest/4/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/4/submission/47877413

---
+++ codeforces/4/A.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(n==2) cout<<"NO"<<endl;
+        else if(n%2==0) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
