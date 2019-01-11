0f0764481c571843cad32188a4d46dccebbb7c51
tstanvir <tanvirshossain0@gmail.com>
2019-01-11 10:59:00 +0600

    Add solution for problem `C - Doors Breaking and Repairing` Link: https://codeforces.com/contest/1102/problem/C Tags: games, *1200 Ref: https://codeforces.com/contest/1102/submission/48200329

---
+++ codeforces/1102/C.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define PI 3.1415926535897932384626433832795
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define SET(x) memset(x, 0, sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define mp make_pair
+#define p printf
+
+#define s scanf
+
+#define ll long long
+
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+#define np std::string::npos
+#define INF Ox3f3f3f3f
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+int main()
+
+{
+	IOS;
+	int n,x,y;
+	while(cin>>n>>x>>y)
+    {
+        int arr[n];
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+            if(arr[i]<=x ) cnt++;
+
+        }
+        if(x>y) cout<<n<<endl;
+        else cout<<ceil((double)cnt/2.0)<<endl;
+    }
+
+	 return 0;
+
+}
+
