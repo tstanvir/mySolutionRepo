140ae9c5fa43910e401d69978ed3946da061432a
tstanvir <tanvirshossain0@gmail.com>
2019-01-11 12:53:00 +0600

    Add solution for problem `A - Elections` Link: https://codeforces.com/contest/1043/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1043/submission/48202847

---
+++ codeforces/1043/A.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define PI 3.1415926535897932384626433832795
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define SET(x) memset(x, 0, sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define mp make_pair
+#define p printf
+
+#define s scanf
+
+#define ll long long
+
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+#define np std::string::npos
+#define INF Ox3f3f3f3f
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+int main()
+
+{
+	IOS;
+	int n;
+	while(cin>>n){
+	int sum=0,maxi=0;
+	int a;
+	for(int i=0;i<n;i++)
+    {
+        cin>>a;
+        sum+=a;
+        if(a>maxi) maxi=a;
+    }
+    if(((maxi*n)-sum)>sum) cout<<maxi<<endl;
+    else cout<<maxi+(2*sum-(maxi*n))/n+1<<endl;
+
+}
+
+
+	 return 0;
+
+}
+
