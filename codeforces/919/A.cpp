57ec10778a547cba14d9dc79294e67288f87cdf7
tstanvir <tanvirshossain0@gmail.com>
2019-01-26 00:06:00 +0600

    Add solution for problem `A - Supermarket` Link: https://codeforces.com/contest/919/problem/A Tags: brute force, greedy, implementation, *800 Ref: https://codeforces.com/contest/919/submission/48943999

---
+++ codeforces/919/A.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define SET(x) memset(x, 0, sizeof(x))
+#define SETBOOL(x) memset(x,false,sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define mp make_pair
+#define p printf
+
+#define s scanf
+
+#define ll long long
+
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0);
+#define np std::string::npos
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+int main()
+
+{
+	IOS;
+	int n,m;
+	while(cin>>n>>m)
+    {
+        double a,b;
+        double arr[n];
+        double mini=1000000;
+        while(n--)
+        {
+            cin>>a>>b;
+            if((a/b)<mini) mini=a/b;
+        }
+
+        cout<<fixed<<setprecision(8)<<mini*m<<endl;
+    }
+
+	 return 0;
+
+}
+
