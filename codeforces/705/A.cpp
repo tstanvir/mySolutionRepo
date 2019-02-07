a41c503b40b340760908222d9e2656acef2e0d1d
tstanvir <tanvirshossain0@gmail.com>
2019-02-08 03:38:00 +0600

    Add solution for problem `A - Hulk` Link: https://codeforces.com/contest/705/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/705/submission/49608964

---
+++ codeforces/705/A.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define SET(x) memset(x, 0, sizeof(x))
+#define SETBOOL(x) memset(x,false,sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define mp make_pair
+#define PII pair<int, int>
+#define p printf
+
+#define s scanf
+
+#define ll long long
+
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0);
+#define np std::string::npos
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+int main()
+
+{
+	IOS;
+	int n;
+	while(cin>>n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            if(i>1) cout<<"that ";
+            if(i%2==1) cout<<"I hate ";
+            else cout<<"I love ";
+        }
+        cout<<"it"<<endl;
+    }
+
+	 return 0;
+
+}
+
