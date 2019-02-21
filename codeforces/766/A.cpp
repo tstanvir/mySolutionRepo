404bb6184d7a4d9a2378af13c9237900f0c12ee0
tstanvir <tanvirshossain0@gmail.com>
2019-02-22 04:20:00 +0600

    Add solution for problem `A - Mahmoud and Longest Uncommon Subsequence` Link: https://codeforces.com/contest/766/problem/A Tags: constructive algorithms, strings, *1000 Ref: https://codeforces.com/contest/766/submission/50297806

---
+++ codeforces/766/A.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define SET(x) memset(x, 0, sizeof(x))
+#define SETBOOL(x) memset(x,false,sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define mp make_pair
+#define PII pair<int, int>
+#define pf printf
+
+#define sf scanf
+
+#define ALL(x) x.begin(),x.end()
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0);
+#define np std::string::npos
+typedef long long ll;
+
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+int main()
+
+{
+	IOS;
+	string s,t;
+	while(cin>>s>>t)
+    {
+        if(s==t) cout<<-1<<endl;
+        else
+        {
+            if(s.size()>t.size()) cout<<s.size()<<endl;
+            else cout<<t.size()<<endl;
+        }
+    }
+
+	 return 0;
+
+}
+
