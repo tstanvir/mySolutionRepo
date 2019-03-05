df7372e33ec172b81cde3ae33ec2423aab140925
tstanvir <tanvirshossain0@gmail.com>
2019-03-05 21:35:00 +0600

    Add solution for problem `A - Regular Bracket Sequence` Link: https://codeforces.com/contest/1132/problem/A Tags: greedy, implementation, *1100 Ref: https://codeforces.com/contest/1132/submission/50835734

---
+++ codeforces/1132/A.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define SET(x) memset(x, 0, sizeof(x))
+#define SETBOOL(x) memset(x,false,sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define mp make_pair
+#define PII pair<int, int>
+#define pf printf
+
+#define sf scanf
+
+#define ALL(x) x.begin(),x.end()
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0);
+#define np std::string::npos
+typedef long long ll;
+
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+int main()
+
+{
+	IOS;
+	int a,b,c,d;
+	while(cin>>a>>b>>c>>d)
+    {
+        if(a==d){
+            if(a==0 and c!=0) cout<<0<<endl;
+            else cout<<1<<endl;
+        }
+        else cout<<0<<endl;
+    }
+
+	 return 0;
+
+}
+
