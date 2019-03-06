e151fcf5ea9fbbf5adc2c1162b943dba19eabe62
tstanvir <tanvirshossain0@gmail.com>
2019-03-06 21:45:00 +0600

    Add solution for problem `A - A Serial Killer` Link: https://codeforces.com/contest/776/problem/A Tags: brute force, implementation, strings, *900 Ref: https://codeforces.com/contest/776/submission/50897498

---
+++ codeforces/776/A.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define SET(x) memset(x, 0, sizeof(x))
+#define SETBOOL(x) memset(x,false,sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define mp make_pair
+#define PII pair<int, int>
+#define pf printf
+
+#define sf scanf
+
+#define ALL(x) x.begin(),x.end()
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0);
+#define np std::string::npos
+typedef long long ll;
+
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+int main()
+
+{
+	IOS;
+	string s,t;
+	while(cin>>s>>t)
+    {
+        map<string,string>m;
+        m[s]=t;
+        m[t]=s;
+        string current=s+" "+t;
+        int n;
+        cin>>n;
+        cout<<current<<endl;
+        while(n--)
+        {
+            string ss,tt;
+            cin>>ss>>tt;
+            current=m[ss]+" "+tt;
+            cout<<current<<endl;
+            m[m[ss]]=tt;
+            m[tt]=m[ss];
+        }
+    }
+
+	 return 0;
+
+}
+
