2a21fb79521ea3b9c989dd54e6d88b10b5ab4888
tstanvir <tanvirshossain0@gmail.com>
2019-06-13 12:11:00 +0600

    Add solution for problem `A - Far Relative’s Birthday Cake` Link: https://codeforces.com/contest/629/problem/A Tags: brute force, combinatorics, constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/629/submission/55517189

---
+++ codeforces/629/A.cpp
@@ -0,0 +1,67 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const long long MOD = 1000000007;
+#define SET(x) memset(x, 0, sizeof(x))
+#define SETBOOL(x) memset(x,false,sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define mp make_pair
+#define PII pair<int, int>
+#define pf printf
+
+#define sf scanf
+
+#define ALL(x) x.begin(),x.end()
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0);
+#define np std::string::npos
+typedef long long ll;
+
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+template<typename T>inline T Bigmod(T base, T power, T MOD){
+    T ret=1;
+    while(power)
+    {
+        if(power & 1)ret=(ret*base)%MOD;
+        base=(base*base)%MOD;
+        power>>=1;
+    }
+    return ret;
+}
+
+int main()
+
+{
+	IOS;
+	int n;
+	while(cin>>n)
+    {
+        string s[n];
+        int ans=0;
+        for(int i=0;i<n;i++) cin>>s[i];
+        for(int i=0;i<n;i++)
+        {
+            int cnt1=0,cnt2=0;
+            for(int j=0;j<n;j++)
+            {
+                if(s[i][j]=='C') cnt1++;
+                if(s[j][i]=='C') cnt2++;
+            }
+            if(cnt1>=2) ans+=((cnt1*(cnt1-1))/2);
+            if(cnt2>=2) ans+=((cnt2*(cnt2-1))/2);
+        }
+        cout<<ans<<endl;
+    }
+
+	 return 0;
+
+}
