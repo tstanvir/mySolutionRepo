e97075bfcd27277a3f9d218e10a603df86c78147
tstanvir <tanvirshossain0@gmail.com>
2019-06-16 16:06:00 +0600

    Add solution for problem `A - Chunga-Changa` Link: https://codeforces.com/contest/1181/problem/A Tags: greedy, math, *1000 Ref: https://codeforces.com/contest/1181/submission/55627867

---
+++ codeforces/1181/A.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const long long MOD = 1000000007;
+#define SET(x) memset(x, 0, sizeof(x))
+#define SETBOOL(x) memset(x,false,sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define mp make_pair
+#define PII pair<int, int>
+#define pf printf
+
+#define sf scanf
+
+#define ALL(x) x.begin(),x.end()
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0);
+#define np std::string::npos
+typedef long long ll;
+
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+template<typename T>inline T Bigmod(T base, T power, T MOD){
+    T ret=1;
+    while(power)
+    {
+        if(power & 1)ret=(ret*base)%MOD;
+        base=(base*base)%MOD;
+        power>>=1;
+    }
+    return ret;
+}
+
+int main()
+
+{
+	IOS;
+	ll x,y,z;
+	while(cin>>x>>y>>z)
+    {
+       ll rem=(x%z)+(y%z);
+        ll bal=(rem/z)*z;
+        ll ans=(x/z)+(y/z)+(rem/z);
+        if((x%z)*(y%z)==0LL || bal==0) cout<<ans<<" "<<0<<endl;
+        else
+        {
+            cout<<ans<<" "<<min(abs(bal-(x%z)),abs(bal-(y%z)))<<endl;
+        }
+    }
+
+	 return 0;
+
+}
+
