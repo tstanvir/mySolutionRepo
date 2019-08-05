afa933589eeb0e283a376344b21657140e346945
tstanvir <tanvirshossain0@gmail.com>
2019-08-06 03:31:00 +0600

    Add solution for problem `C - Maximum Median` Link: https://codeforces.com/contest/1201/problem/C Tags: binary search, greedy, math, sortings, *1400 Ref: https://codeforces.com/contest/1201/submission/58358764

---
+++ codeforces/1201/C.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const long long MOD = 1000000007;
+#define SET(x) memset(x, 0, sizeof(x))
+#define SET2d(x,m,n) memset(x, 0, sizeof(x[0][0]) * m * n)
+#define SETBOOL(x) memset(x,false,sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define mp make_pair
+#define PII pair<int, int>
+#define pf printf
+
+#define sf scanf
+
+#define ALL(x) x.begin(),x.end()
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0);
+#define np std::string::npos
+#define for0(i,n) for(int i=0;i<n;i++)
+#define forn(i,n) for(int i=n-1;i>=0;i--)
+#define highest(x) numeric_limits<x>::max()
+#define lowest(x) numeric_limits<x>::min()
+#define Inf INFINITY
+typedef long long ll;
+
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+template<typename T>inline T Bigmod(T base, T power, T MOD){
+    T ret=1;
+    while(power)
+    {
+        if(power & 1)ret=(ret*base)%MOD;
+        base=(base*base)%MOD;
+        power>>=1;
+    }
+    return ret;
+}
+ll n,k;vector<ll>vec;
+bool ok(ll mid){
+    ll sum=0;
+    for(int i=0;i<vec.size();i++){
+        sum+=max(0ll,(mid-vec[i]));
+        if(sum>k) return 0;
+    }
+  //  cout<<sum<<endl;
+    return 1;
+
+}
+int main()
+
+{
+	IOS;
+
+	while(cin>>n>>k){
+        vec.clear();
+
+        for(int i=0;i<n;i++){
+            int a;
+            cin>>a;
+            vec.pb(a);
+        }
+        sort(ALL(vec));
+        vec.erase(vec.begin(),vec.begin()+(n/2));
+        ll lo=vec[0],hi=2*1000*1000*1000;
+        while(hi>lo){
+               // cout<<hi<<" "<<lo<<endl;
+            ll mid=(lo+hi+1)/2;
+            if(ok(mid)){
+                lo=mid;
+                //break;
+            }
+            else hi=mid-1;
+
+        }
+        cout<<lo<<endl;
+	}
+
+	 return 0;
+
+}
+
