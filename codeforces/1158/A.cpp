04ecc8596266eca50a1c679c3a7ab16ecb5fc48a
tstanvir <tanvirshossain0@gmail.com>
2019-11-29 12:40:00 +0600

    Add solution for problem `A - The Party and Sweets` Link: https://codeforces.com/contest/1158/problem/A Tags: binary search, constructive algorithms, greedy, implementation, math, sortings, two pointers, *1500 Ref: https://codeforces.com/contest/1158/submission/65941825

---
+++ codeforces/1158/A.cpp
@@ -0,0 +1,89 @@
+/// Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const long long MOD = 1000000007;
+#define SET(x) memset(x, 0, sizeof(x))
+#define SET2d(x,m,n) memset(x, 0, sizeof(x[0][0]) * m * n)
+#define SETBOOL(x) memset(x,false,sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define mp make_pair
+#define PII pair<int, int>
+#define pf printf
+
+#define sf scanf
+
+#define ALL(x) x.begin(),x.end()
+#define pb push_back
+
+#define IOS ios::sync_with_stdio(false); cin.tie(0);
+#define np std::string::npos
+#define for0(i,n) for(int i=0;i<n;i++)
+#define forn(i,n) for(int i=n-1;i>=0;i--)
+#define highest(x) numeric_limits<x>::max()
+#define lowest(x) numeric_limits<x>::min()
+#define Inf INFINITY
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define cases(cs,t) for(int cs=1;cs<=t;cs++)
+#define PI acos(-1)
+typedef long long ll;
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[] = {0, 0, 1, -1};
+int dy4[] = {1, -1, 0, 0};
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
+template<typename T>inline T Bigmod(T base, T power, T MOD){
+    T ret=1;
+    while(power)
+    {
+        if(power & 1)ret=(ret*base)%MOD;
+        base=(base*base)%MOD;
+        power>>=1;
+    }
+    return ret;
+}
+
+int main()
+
+{
+	IOS;
+	//freopen("input.txt", "r", stdin);
+     //freopen("output.txt", "w", stdout);
+    int n,m;
+    cin>>n>>m;
+    ll sumb=0;
+    ll sumg = 0;
+    ll maxb=-1,ming = highest(ll);
+    int bou[n];
+    for(int i=0;i<n;i++){
+        ll x;
+        cin>>x;
+        sumb+=x;
+        bou[i] = x;
+    }
+    sort(bou,bou+n);
+    ll max2b = bou[n-2];
+    maxb = bou[n-1];
+    for(int i=0;i<m;i++){
+        ll x;
+        cin>>x;
+        sumg+=x;
+        ming = min(x,ming);
+    }
+    if(maxb>ming){cout << "-1\n";}
+    else if(maxb==ming){cout << ((sumb*m)+sumg-(maxb*m))<<endl;}
+    else{
+        cout << ((sumb*m)+sumg - (maxb*(m-1)) - max2b)<<endl;
+    }
+	 return 0;
+
+}
+///Alhamdulillah
