64e1da2c39cf5bfc01c54bc1dd18af3b6b2715f5
tstanvir <tanvirshossain0@gmail.com>
2020-06-02 23:44:00 +0600

    Add solution for problem `A - Sum of Odd Integers` Link: https://codeforces.com/contest/1327/problem/A Tags: math, *1100 Ref: https://codeforces.com/contest/1327/submission/82334545

--- codeforces/1327/A.cpp
+++ codeforces/1327/A.cpp
@@ -76,15 +76,40 @@ double sq(double x) {return x*x;}
 void solve(){
     ll n,k;
     cin>>n>>k;
-    if((ll)sq(k)>n){
-        cout<<"NO"<<endl;
-        return;
+    if(k<=(n+1)/2){
+        if(k==1){
+            if(n%2) cout<<"YES"<<endl;
+            else cout<<"NO"<<endl;
+        }
+        else if(k==2){
+            if(n%2) cout<<"NO"<<endl;
+            else cout<<"YES"<<endl;
+        }
+        else{
+            if(n%2==0){
+                if(k%2) cout<<"NO"<<endl;
+                else{
+                    if(k<=(ll)sqrt(n)){
+                        cout<<"YES"<<endl;
+                    }
+                    else cout<<"NO"<<endl;
+                }
+            }
+            else{
+                if(k%2==0) cout<<"NO"<<endl;
+                else{
+                    if(k<=(ll)sqrt(n)){
+                        cout<<"YES"<<endl;
+                    }
+                    else cout<<"NO"<<endl;
+                }
+
+            }
+        }
     }
-    if(n%2!=k%2){
+    else{
         cout<<"NO"<<endl;
-        return;
     }
-    cout<<"YES"<<endl;
 }
 
 signed main()
