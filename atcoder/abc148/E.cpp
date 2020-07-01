d079885c29718fbc8222606fa24ef9f1b1dbf468
tstanvir <tanvirshossain0@gmail.com>
2020-07-02 03:42:00 +0600

    Add solution for problem `E - Double Factorial` Link: https://atcoder.jp/contests/abc148/tasks/abc148_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc148/submissions/14893514

--- atcoder/abc148/E.cpp
+++ atcoder/abc148/E.cpp
@@ -1,59 +1,129 @@
- //BISMILLAHIR RAHMANIR RAHEEM
-//ALLAH IS WATCHING ME
+/// Bismillahir Rahmanir Rahim
+//Author: Tanvir Hussain
+//ICE,NSTU
 #include<bits/stdc++.h>
-#include<cstring>
-#include<cmath>
-#include<iterator>
-#include<cstdlib>
-#define IOS   ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define test  long long t;cin >> t;while(t--)
-#define ses   "\n"
-#define whp   " "
-#define mxi 1000000
-#define REP(i,a,b) for(int i=a; i<b; i++)
-#define rep0(i,n) REP(i,0,n)
-#define rep1(i,n) REP(i,1,n)
-# define INF 0x3f3f3f3f
-//set <int, greater <int> > ::iterator it;
-//vector<pair<int,int> >a;
-//memset(arr,0,sizeof(hg));
-//priority_queue <int, vector<int>, greater<int>> pq;
-///  string single character erase-
-///             s.erase(s.begin()+x); where s is st ring name
-///               ans x is index;
-typedef long long v99;
-typedef unsigned long long ull;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
-v99 fx[4]= {1,-1,0,0};
-v99 fy[4]= {0,0,1,-1};
+const long long MOD = 1000000007;
+#define SET(x) memset(x, 0, sizeof(x))
+#define SET2d(x,m,n) memset(x, 0, sizeof(x[0][0]) * m * n)
+#define SETBOOL(x) memset(x,false,sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define CLR2d(x,m,n) memset(x, -1, sizeof(x[0][0]) * m * n)
+#define mp make_pair
+#define PII pair<int, int>
+#define pf printf
+#define sf scanf
+#define ALL(x) x.begin(),x.end()
+#define pb push_back
+#define IOS ios::sync_with_stdio(false); cin.tie(0);
+#define np std::string::npos
+#define highest(x) numeric_limits<x>::max()
+#define lowest(x) numeric_limits<x>::min()
+#define Inf INFINITY
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define cases(cs,t) for(int cs=1;cs<=t;cs++)
+#define PI acos(-1)
+#define no1 __builtin_popcount
+#define BOUNDARY(i, j) ((i >= 0 && i < row) && (j >= 0 && j < column))
+#define uniq(vec) vec.resize(distance(vec.begin(),unique(vec.begin(),vec.end())))
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define endl "\n"
+#define forch(it,s) for(auto it:s)
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define bits(n) __builtin_popcount(n)
+#define maxpq priority_queue<int>
+#define minpq priority_queue<int, vector<int>, greater<int> >
 
-long double power(long double p,long double q)
+
+int gcd(int a, int b) { if (a == 0) return b; return gcd(b % a, a);}
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pair<int,int>> vpii;
+typedef tree<pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[] = {0, 0, 1, -1};
+int dy4[] = {1, -1, 0, 0};
+const int maxx=100005;
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
 {
-    long double ans=pow(p,q);
+    return a.first>b.first;
+}
+
+template<typename T>inline T Bigmod(T base, T power, T MOD){
+    T ret=1;
+    while(power)
+    {
+        if(power & 1)ret=(ret*base)%MOD;
+        base=(base*base)%MOD;
+        power>>=1;
+    }
+    return ret;
+}
+double sq(double x) {return x*x;}
+ll po(ll b,ll p){
+    ll res=1;
+    while(p){
+        res*=b;
+        p--;
+    }
+    return res;
+}
+ll lg2(ll x){
+    ll res=0;
+    while(x>1){
+        res++;
+        x/=2ll;
+    }
+    return res;
+}
+ll zeroes(ll x){
+    ll p=5;
+    ll ans=0;
+    while((x/p)>0){
+        ans+=((x/p)/2ll);
+        //if(x&1) ans++;
+        p*=5;
+    }
     return ans;
 }
-
-v99 koita_ache(v99 n,v99 base)
-{
-    v99 kot=0;
-    while(n)
-    {
-        n/=base;
-        kot+=n/2;
-    }
-    return kot;
+void solve(){
+    ll n;
+    cin>>n;
+    if(n&1) cout<<0<<endl;
+    else cout<<zeroes(n)<<endl;
 }
-int main()
-{
-    //v99 k=1;
-    //test
-    //while(1)
-    {
-        v99 n;cin>>n;
-        if(n&1)cout<<0<<ses;
-        else cout<<koita_ache(n,5)<<ses;
 
-        //cout<<min(ache_2,ache_5)<<ses;
+signed main()
+
+{
+    IOS;
+    /*#ifndef ONLINE_JUDGE
+        freopen ("data.in","r",stdin);
+        freopen ("data.out","w",stdout);
+    #endif*/
+    int t;
+    t=1;
+    while(t--){
+        solve();
     }
-    return 0;
+
+     return 0;
+
 }
+///Alhamdulillah
+
+
