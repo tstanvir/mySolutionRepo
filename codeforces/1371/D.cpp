ad4e06fcc49e86413c30f70fba81eb6bf303d43f
tstanvir <tanvirshossain0@gmail.com>
2020-07-02 06:59:00 +0600

    Add solution for problem `D - Grid-00100` Link: https://codeforces.com/contest/1371/problem/D Tags: constructive algorithms, greedy, implementation, *1600 Ref: https://codeforces.com/contest/1371/submission/85725948

--- codeforces/1371/D.cpp
+++ codeforces/1371/D.cpp
@@ -17,7 +17,7 @@ const long long MOD = 1000000007;
 #define pf printf
 #define sf scanf
 #define ALL(x) x.begin(),x.end()
-#define pb push_back;
+#define pb push_back
 #define IOS ios::sync_with_stdio(false); cin.tie(0);
 #define np std::string::npos
 #define highest(x) numeric_limits<x>::max()
