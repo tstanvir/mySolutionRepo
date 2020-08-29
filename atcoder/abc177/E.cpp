7985e6e04cb9ca9b1b0ccf9a39a62d89781e110d
tstanvir <tanvirshossain0@gmail.com>
2020-08-29 18:43:00 +0600

    Add solution for problem `E - Coprime` Link: https://atcoder.jp/contests/abc177/tasks/abc177_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc177/submissions/16346390

--- atcoder/abc177/E.cpp
+++ atcoder/abc177/E.cpp
@@ -1,49 +1,70 @@
 /// Bismillahir Rahmanir Rahim
-/* Mohammad Morsalin
-   Dept of ICE, NSTU
-*/
+//Author: Tanvir Hussain
+//ICE,NSTU
 #include<bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
+using namespace std;
+const long long MOD = 1000000007;
 #pragma GCC optimize("Ofast")
 #pragma GCC target("avx,avx2,fma")
-#define ll long long
-#define pb push_back
+#define SET(x) memset(x, 0, sizeof(x))
+#define SET2d(x,m,n) memset(x, 0, sizeof(x[0][0]) * m * n)
+#define SETBOOL(x) memset(x,false,sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define CLR2d(x,m,n) memset(x, -1, sizeof(x[0][0]) * m * n)
 #define mp make_pair
-#define endl "\n"
-#define int long long
-#define f0(n) for(int i=0;i<n;i++)
-#define ms(x) memset(x,0,sizeof(x))
-#define ms2d(x,m,n) memset(x, 0, sizeof(x[0][0]) * m * n)
-#define uniq(vec) vec.resize(distance(vec.begin(),unique(vec.begin(),vec.end())))
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define pi pair<int,int>
-#define tc(t) int t;cin>>t;while(t--)
-#define bits(n) __builtin_popcount(n)
-#define maxpq priority_queue<int>
-#define minpq priority_queue<int, vector<int>, greater<int> >
-#define ins insert
-#define ALL(v) v.begin(),v.end()
+#define PII pair<int, int>
+#define pf printf
+#define sf scanf
+#define ALL(x) x.begin(),x.end()
+#define pb push_back
+#define IOS ios::sync_with_stdio(false); cin.tie(0);
+#define np std::string::npos
 #define highest(x) numeric_limits<x>::max()
 #define lowest(x) numeric_limits<x>::min()
 #define Inf INFINITY
 #define minv(v) *min_element(v.begin(),v.end())
 #define maxv(v) *max_element(v.begin(),v.end())
-#define fi first
-#define se second
+#define cases(cs,t) for(int cs=1;cs<=t;cs++)
 #define PI acos(-1)
-#define sz(a) (int)a.size();
-#define IOS ios::sync_with_stdio(false);
-using namespace std;
+#define no1 __builtin_popcount
+#define BOUNDARY(i, j) ((i >= 0 && i < row) && (j >= 0 && j < column))
+#define uniq(vec) vec.resize(distance(vec.begin(),unique(vec.begin(),vec.end())))
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define endl "\n"
+#define forch(it,s) for(auto it:s)
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define bits(n) __builtin_popcount(n)
+#define maxpq priority_queue<int>
+#define minpq priority_queue<int, vector<int>, greater<int> >
+
+
 int gcd(int a, int b) { if (a == 0) return b; return gcd(b % a, a);}
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pair<int,int>> vpii;
 typedef tree<pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
 int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
 int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
 int dx4[] = {0, 0, 1, -1};
 int dy4[] = {1, -1, 0, 0};
-const long long MOD = 1000000007;
-double sq(double x) {return x*x;}
+const int maxx=100005;
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+
 template<typename T>inline T Bigmod(T base, T power, T MOD){
     T ret=1;
     while(power)
@@ -54,62 +75,86 @@ template<typename T>inline T Bigmod(T base, T power, T MOD){
     }
     return ret;
 }
-
-bool sortinrev(const pair<int,int> &a,
-               const pair<int,int> &b)
-{
-       return (a.first > b.first);
+double sq(double x) {return x*x;}
+ll po(ll b,ll p){
+    ll res=1;
+    while(p){
+        res*=b;
+        p--;
+    }
+    return res;
 }
-signed main()
-{
-    IOS
+ll lg2(ll x){
+    ll res=0;
+    while(x>1){
+        res++;
+        x/=2ll;
+    }
+    return res;
+}
+void solve(){
     int n;
     cin>>n;
-    vector<int>vec(n);
-    for(int i=0;i<n;i++){
-        cin>>vec[i];
-    }
-    map<int,int>mmp;
-    bool pd=0;
-    for(int i=0;i<n;i++){
-        int v=vec[i];
-        for(int j=2;j*j<=v;j++){
-            int gun=0;
-            while(v%j==0) {
-                gun++;
-                v/=j;
+    vi vec(n);
+    rep(i,n) cin>>vec[i];
+    map<int,int>mm;
+    bool ok=0;
+    rep(i,n){
+        int val=vec[i];
+
+        for(int j=2;j*j<=val;j++){
+            int cnt=0;
+            while(val%j==0) {
+                cnt++;
+                val/=j;
             }
-            if(gun){
-                if(mmp[j]){
-                    pd=1;
+            if(cnt){
+                if(mm[j]){
+                    ok=1;
                 }
-                else mmp[j]=1;
+                else mm[j]=1;
             }
 
         }
-        if(v>1){
-            if(mmp[v]){
-                pd=1;
+        if(val>1){
+            if(mm[val]){
+                ok=1;
             }
-            else mmp[v]=1;
+            else mm[val]=1;
         }
     }
-    if(!pd){
-        cout << "pairwise coprime\n";
+    if(!ok){
+        cout<<"pairwise coprime"<<endl;
+        return;
     }
-    else{
-        int gc=vec[0];
-        for(int i=1;i<n;i++){
-            gc=gcd(gc,vec[i]);
-        }
-        if(gc==1){
-            cout << "setwise coprime\n";
-        }
-        else{
-            cout << "not coprime\n";
-        }
+    int Gcd=vec[0];
+    rep1(i,1,n-1){
+        Gcd=gcd(Gcd,vec[i]);
+    }
+    if(Gcd==1){
+        cout<<"setwise coprime"<<endl;
+        return;
+    }
+    cout<<"not coprime"<<endl;
+}
+
+signed main()
+
+{
+    IOS;
+    /*#ifndef ONLINE_JUDGE
+        freopen ("input.txt","r",stdin);
+        freopen ("output.txt","w",stdout);
+    #endif*/
+    int t;
+    t=1;
+    while(t--){
+        solve();
     }
 
-    return 0;
+     return 0;
+
 }
 ///Alhamdulillah
+
+
