80aec9a0054015c701f4f6d8ca9b727d2e91dbf5
tstanvir <tanvirshossain0@gmail.com>
2020-10-25 03:36:00 +0600

    Add solution for problem `B - Values` Link: https://atcoder.jp/contests/arc106/tasks/arc106_b Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/arc106/submissions/17645222

---
+++ atcoder/arc106/B.cpp
@@ -0,0 +1,185 @@
+/// Bismillahir Rahmanir Rahim
+//Author: Tanvir Hussain
+//ICE,NSTU
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+const long long MOD = 1000000007;
+const double rad=(acos(-1)/180.00);
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#define SET(x) memset(x, 0, sizeof(x))
+#define SET2d(x,m,n) memset(x, 0, sizeof(x[0][0]) * m * n)
+#define SETBOOL(x) memset(x,false,sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define CLR2d(x,m,n) memset(x, -1, sizeof(x[0][0]) * m * n)
+#define mp make_pair
+#define PII pair<int, int>
+#define pf printf
+#define sf scanf
+#define ALL(x) x.begin(),x.end()
+#define pb push_back
+#define IOS ios::sync_with_stdio(false); cin.tie(0);
+#define np std::string::npos
+#define highest(x) numeric_limits<x>::max()
+#define lowest(x) numeric_limits<x>::min()
+#define Inf INFINITY
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define cases(cs,t) for(int cs=1;cs<=t;cs++)
+#define PI acos(-1)
+#define no1 __builtin_popcount
+#define BOUNDARY(i, j) ((i >= 0 && i < row) && (j >= 0 && j < column))
+#define uniq(vec) vec.resize(distance(vec.begin(),unique(vec.begin(),vec.end())))
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define endl "\n"
+#define forch(it,s) for(auto it:s)
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define bits(n) __builtin_popcount(n)
+#define maxpq priority_queue<int>
+#define minpq priority_queue<int, vector<int>, greater<int> >
+
+
+
+int gcd(int a, int b) { if (a == 0) return b; return gcd(b % a, a);}
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pair<int,int>> vpii;
+typedef tree<pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[] = {0, 0, 1, -1};
+int dy4[] = {1, -1, 0, 0};
+const int maxx=200005;
+
+inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
+inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
+inline ll modAdd(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a+b)%MOD; }
+inline ll modSub(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while(p) { if(p&1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, MOD-2); }  /// When MOD is prime.
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+
+
+
+//this fuction sorts vector pair according to first element in descending order.
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return (a.ff<b.ff) or (a.ss<b.ff);
+}
+
+template<typename T>inline T Bigmod(T base, T power, T MOD){
+    T ret=1;
+    while(power)
+    {
+        if(power & 1)ret=(ret*base)%MOD;
+        base=(base*base)%MOD;
+        power>>=1;
+    }
+    return ret;
+}
+double sq(double x) {return x*x;}
+ll po(ll b,ll p){
+    ll res=1;
+    while(p){
+        res*=b;
+        p--;
+    }
+    return res;
+}
+ll lg2(ll x){
+    ll res=0;
+    while(x>1){
+        res++;
+        x/=2ll;
+    }
+    return res;
+}
+vll a(maxx),b(maxx);
+struct DSU{
+    vector <int> arr, szz;//p[i]=j means j is the root of  i; //
+    DSU(int n) {
+        arr.resize(n + 1); szz.resize(n + 1, 1);
+        for(int i = 1; i <= n; i++) {
+            arr[i] = i;
+        }
+    }
+    int root(int i) {
+        while(i!=arr[i]){
+            arr[i]=arr[arr[i]];
+            i=arr[i];
+        }
+        return i;
+    }
+    int merge(int a, int b) {
+        int root_a=root(a),root_b=root(b);
+        if(root_a==root_b) return 1;
+        if(szz[root_a]<szz[root_b]){
+            arr[root_a]=arr[root_b];
+            szz[root_b]+=szz[root_a];
+        }
+        else{
+            arr[root_b]=arr[root_a];
+            szz[root_a]+=szz[root_b];
+        }
+        return 1;
+    }
+};
+void solve(){
+    int n,m;
+    cin>>n>>m;
+
+    rep1(i,1,n) cin>>a[i];
+    rep1(i,1,n) cin>>b[i];
+    DSU d(n);
+    rep(i,m){
+        int u,v;
+        cin>>u>>v;
+        d.merge(u,v);
+    }
+    vll c(n+1);
+    rep1(i,1,n){
+        int rt=d.root(i);
+        c[rt]+=a[i]-b[i];
+    }
+    rep1(i,1,n){
+        //cout<<d.find(i)<<" "<<c[i]<<endl;
+        if(d.root(i)==i and c[i]){
+            cout<<"No"<<endl;
+            return;
+        }
+    }
+    cout<<"Yes"<<endl;
+}
+
+signed main()
+
+{
+    IOS;
+    /*#ifndef ONLINE_JUDGE
+        freopen ("input.txt","r",stdin);
+        freopen ("output.txt","w",stdout);
+    #endif*/
+    int t;
+    t=1;
+    while(t--){
+        solve();
+    }
+
+     return 0;
+
+}
+///Alhamdulillah
+
+
+
