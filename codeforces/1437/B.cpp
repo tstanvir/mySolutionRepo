2f74379939994cf3c06d95d460e1bc3c06869756
tstanvir <tanvirshossain0@gmail.com>
2020-10-28 00:44:00 +0600

    Add solution for problem `B - Reverse Binary Strings` Link: https://codeforces.com/contest/1437/problem/B Tags: constructive algorithms, greedy, *1200 Ref: https://codeforces.com/contest/1437/submission/96936171

--- codeforces/1437/B.cpp
+++ codeforces/1437/B.cpp
@@ -48,29 +48,6 @@ const double rad=(acos(-1)/180.00);
 #define minpq priority_queue<int, vector<int>, greater<int> >
 
 
-vector<string> vec_splitter(string s) {
-	s += ',';
-	vector<string> res;
-	while(!s.empty()) {
-		res.push_back(s.substr(0, s.find(',')));
-		s = s.substr(s.find(',') + 1);
-	}
-	return res;
-}
-void debug_out(
-vector<string> __attribute__ ((unused)) args,
-__attribute__ ((unused)) int idx,
-__attribute__ ((unused)) int LINE_NUM) { cerr << endl; }
-template <typename Head, typename... Tail>
-void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
-	if(idx > 0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
-	stringstream ss; ss << H;
-	cerr << args[idx] << " = " << ss.str();
-	debug_out(args, idx + 1, LINE_NUM, T...);
-}
-
-
-
 
 int gcd(int a, int b) { if (a == 0) return b; return gcd(b % a, a);}
 typedef long long ll;
@@ -128,20 +105,11 @@ ll lg2(ll x){
     }
     return res;
 }
-#define XOX
-#ifdef XOX
-#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
-#else
-#define debug(...) 42
-#endif
-
-
 void solve(){
     int n;
     cin>>n;
     string s;
     cin>>s;
-    debug(n,s);
     int one=0,zero=0;
     int eq=0;
     rep1(i,1,n-1){
