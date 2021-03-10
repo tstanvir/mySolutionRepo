06f731c33eb1cbcd595aba2fc1fd45e69c7556ea
tstanvir <tanvirshossain0@gmail.com>
2021-03-11 05:55:00 +0600

    Add solution for problem `C - Maximum width` Link: https://codeforces.com/contest/1492/problem/C Tags: binary search, data structures, dp, greedy, two pointers, *1500 Ref: https://codeforces.com/contest/1492/submission/109662290

--- codeforces/1492/C.cpp
+++ codeforces/1492/C.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-
 using namespace __gnu_pbds;
 using namespace std;
 
@@ -127,6 +126,7 @@ void solve(){
     }
     cout<<ans<<endl;
 }
+ 
 signed main()
  
 {
