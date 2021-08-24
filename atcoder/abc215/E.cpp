5a0249d3a741739634a7d132447deb3acaeda534
tstanvir <tanvirshossain0@gmail.com>
2021-08-24 11:50:00 +0600

    Add solution for problem `E - Chain Contestant` Link: https://atcoder.jp/contests/abc215/tasks/abc215_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc215/submissions/25309410

---
+++ atcoder/abc215/E.cpp
@@ -0,0 +1,199 @@
+// Problem: E - Chain Contestant
+// Contest: AtCoder - AtCoder Beginner Contest 215
+// URL: https://atcoder.jp/contests/abc215/tasks/abc215_e
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//find_by_order(k): It returns to an iterator to the kth element 
+//(counting from zero) in the set in O(logn) time.
+//To find the first element k must be zero.
+
+// order_of_key(k) : It returns to the number of items that 
+// are strictly smaller than our item k in O(logn) time.
+
+//using namespace __gnu_pbds;
+using namespace std;
+
+//#pragma comment(linker, "/stack:200000000")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+#define IOS ios::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+#define SET(x) memset(x, 0, sizeof(x))
+#define CLR(x) memset(x, -1, sizeof(x))
+#define mp make_pair
+#define ALL(x) x.begin(),x.end()
+#define pb push_back
+#define ppb pop_back
+#define highest(x) numeric_limits<x>::max()
+#define lowest(x) numeric_limits<x>::min()
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define PI acos(-1)
+#define border(i, j , row, column) ((i >= 0 && i < row) && (j >= 0 && j < column))
+#define uniq(vec) vec.resize(distance(vec.begin(),unique(vec.begin(),vec.end())))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define endl "\n"
+#define forch(it,s) for(auto it:s)
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define no1(n) __builtin_popcount(n)
+#define maxpq priority_queue<int>
+#define minpq priority_queue<int, vector<int>, greater<int> >
+#define di deque<int>
+#define dll deque<ll>
+#define pf push_front
+#define ppf pop_front
+#define preci cout<<fixed<<setprecision(9);
+
+ 
+ 
+vector<string> vec_splitter(string s) {
+    s += ',';
+    vector<string> res;
+    while(!s.empty()) {
+        res.push_back(s.substr(0, s.find(',')));
+        s = s.substr(s.find(',') + 1);
+    }
+    return res;
+}
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx,
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+    if(idx > 0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+    stringstream ss; ss << H;
+    cerr << args[idx] << " = " << ss.str();
+    debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#define XOX
+#ifdef XOX
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+ 
+ 
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pair<int,int>> vpii;
+typedef vector<pair<ll,ll>> vpll;
+//typedef tree<pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> omst;
+//typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> ost; 
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[] = {0, 0, 1, -1};
+int dy4[] = {1, -1, 0, 0};
+const int maxx=1<<10;
+const long long MOD = 998244353;
+const double rad=(acos(-1)/180.00);
+const int INF    = 0x3f3f3f3f;
+const ll LL_INF  = 0x3f3f3f3f3f3f3f3f;
+const ll negInf=-1e18;
+const ll posInf=1e18;
+#define EPS  0.000000001
+
+ 
+inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
+inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
+inline ll modAdd(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a+b)%MOD; }
+inline ll modSub(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while(p) { if(p&1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, MOD-2); }  /// When MOD is prime.
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+ll gcd(ll a, ll b) { if (a == 0) return b; return gcd(b % a, a);}
+ll lcm(ll a, ll b) {return (a*b)/gcd(a,b);}
+double sq(double x) {return x*x;} 
+ll po(ll b,ll p){ ll res=1; while(p){ res*=b; p--;} return res;}
+ll lg2(ll x){ ll res=0; while(x>1){ res++; x/=2ll;} return res;}
+bool get_bit(int mask,int pos) {return mask&(1<<pos);}
+bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.first>b.first;
+}
+int cs=0;
+int n;
+string s;
+ll dp[maxx][10][maxx];//[ith index,type,set of types]
+ll sol(int ind,int t,int mask){
+	if(ind==n){
+		return mask>0;//base case: if there is a single type in the set,
+						// return 1. otherwise 0.
+	}
+	ll &res=dp[ind][t][mask];
+	if(res!=-1) return res;
+	res=0;
+	res+=sol(ind+1,t,mask);//without taking ith indexed type
+	normal(res);
+	int ch=s[ind]-'A';
+	if(ch==t){
+		res+=sol(ind+1,t,(mask|(1<<t)));//if previously taken type 
+		normal(res);						//matches the ith type, take it
+	}
+	else if((mask&(1<<ch))==0){
+		res+=sol(ind+1,ch,(mask|(1<<ch)));//if previously taken type does
+										//not match the ith type and it 
+										//does not exist in the set or mask
+										//take it.
+	}
+	normal(res);
+	return res;
+}
+void solve(){
+    //cout<<"Case "<<++cs<<": ";
+    cin>>n>>s;
+    //CLR(dp);
+    //cout<<sol(0,0,0)<<endl;
+    rep1(i,1,n){
+    	rep(u,maxx){
+    		rep(t,10){
+    			dp[i][t][u]=modAdd(dp[i][t][u],dp[i-1][t][u]);
+    			if(t==s[i-1]-'A'){
+    				dp[i][t][u]=modAdd(dp[i][t][u],dp[i-1][t][u]);
+    			}
+    			else if((u&(1<<(s[i-1]-'A')))==0){
+    				dp[i][s[i-1]-'A'][u|(1<<(s[i-1]-'A'))]=modAdd(dp[i][s[i-1]-'A'][u|(1<<(s[i-1]-'A'))],dp[i-1][t][u]);
+    			}
+    		}
+    	}
+    	dp[i][s[i-1]-'A'][(1<<(s[i-1]-'A'))]++;
+    	normal(dp[i][s[i-1]-'A'][(1<<(s[i-1]-'A'))]);
+    }
+    ll ans=0;
+    rep1(u,0,maxx-1){
+    	rep(t,10){
+    		ans=modAdd(ans,dp[n][t][u]);
+    	}
+    }
+    cout<<ans<<endl;
+}
+ 
+signed main()
+ 
+{
+    IOS;
+    int t;
+    t=1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+ 
+}
